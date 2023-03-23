cf4a5812450774582fae21f6fe2b7fa93e65c7ad
Jon Uleis <hi@jonuleis.com>
2023-03-23 13:43:14 -0400

    Port userscript to extension

---
+++ scripts/content.js
@@ -0,0 +1,54 @@
+// Copyright 2023 Jon Uleis
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+const priceSort = document.querySelector(
+  ".price_header .sortable_link_selected"
+);
+const ascending = priceSort?.title.includes("ascending");
+
+function tableSort() {
+  // continue only if we are sorting by price
+  if (priceSort) {
+    const rows = Array.from(document.querySelectorAll("tr[data-release-id]"));
+    rows.sort((rowA, rowB) => {
+      const priceA = getRowPrice(rowA);
+      const priceB = getRowPrice(rowB);
+      return ascending ? priceA - priceB : priceB - priceA;
+    });
+    // append back to table in new order
+    rows.forEach((row) => row.parentNode.appendChild(row));
+    // change title text
+    priceSort.querySelector(".link-text").innerText = "Total Price";
+  }
+}
+
+function getRowPrice(row) {
+  // if there's no total price, get the original bold one
+  const price =
+    row.querySelector(".converted_price") || row.querySelector(".price");
+  // if the item is unavailable and we're sorting by lowest, push to bottom
+  const weight =
+    row.querySelector(".item_add_to_cart .button") || !ascending ? 0 : 9999999;
+  // strip everything else out of the price text
+  return parseFloat(price.textContent.replace(/[^0-9]/g, "")) + weight;
+}
+
+const tableBlock = document.querySelector(".table_block");
+const observer = new MutationObserver((mutationsList) => {
+  for (let mutation of mutationsList) {
+    // run function again if we're ajax loading in another table of items
+    if (
+      mutation.type === "childList" &&
+      mutation.addedNodes.length &&
+      mutation.addedNodes[0].nodeName === "TBODY"
+    ) {
+      tableSort();
+    }
+  }
+});
+observer.observe(tableBlock, { childList: true });
+
+// first run
+tableSort();
